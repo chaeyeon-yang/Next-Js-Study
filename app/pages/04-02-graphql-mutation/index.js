41bb95788549485489098080afdbabb985c8bb0f
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-07-04 18:29:33 +0900

    Feat: apollo-client 사용 (gql)

---
+++ app/pages/04-02-graphql-mutation/index.js
@@ -0,0 +1,21 @@
+import { gql, useMutation } from "@apollo/client";
+
+const CREATE_BOARD = gql`
+    mutation {
+        createBoard(writer: "채연", title: "하잉", contents: "내용이에유") {
+            _id
+            number
+            message
+        }
+    }
+`;
+
+export default function GraphqlMutationPage() {
+    const [나의함수] = useMutation(CREATE_BOARD);
+    const onClickSubmit = async () => {
+        const result = await 나의함수();
+        console.log(result);
+        alert(result.data.createBoard.message);
+    };
+    return <button onClick={onClickSubmit}>GRAPHQL-API(동기) 요청하기</button>;
+}
