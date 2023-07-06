dcb13655eae69d3f7748135ca5efe1ed67757be7
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-07-06 15:17:32 +0900

    Feat: 비동기 통신과 리렌더링

---
+++ app/pages/05-06-static-routed-board-query/3/index.js
@@ -0,0 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+
+const FETCH_BOARD = gql`
+    query fetchBoard($number: Int) {
+        fetchBoard(number: $number) {
+            writer
+            title
+            contents
+        }
+    }
+`;
+
+// useMutation은 나의 함수로 만들어서 실행시키고 싶은 곳에서 실행시킬 수 있음
+// useQuery는 페이지에 들어오자마자 자동으로 실행되어 백엔드에 api 요청이 들어간다.
+
+export default function index() {
+    const { data } = useQuery(FETCH_BOARD, {
+        variables: {
+            number: 1449,
+        },
+    });
+    console.log(data);
+
+    return (
+        <>
+            <div>3번 게시물로 이동이 완료되었습니다.</div>
+            <div>
+                작성자: {data ? data.fetchBoard.writer : "로딩중입니다..."}
+            </div>
+            <div>제목: {data && data.fetchBoard.title}</div>
+            <div>내용: {data?.fetchBoard.contents}</div>
+            {/* 옵셔널 체이닝 */}
+        </>
+    );
+}
