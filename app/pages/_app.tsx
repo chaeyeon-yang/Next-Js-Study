ed16ad0c087c094d1d321990dbdc8825dfed93b1
ChaeyeonYang <69382168+chaeyeon-yang@users.noreply.github.com>
2023-07-13 22:53:36 +0900

    Chore: typescript 환경설정

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,13 +1,14 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component }: AppProps) {
     const client = new ApolloClient({
         uri: "http://practice.codebootcamp.co.kr/graphql",
         cache: new InMemoryCache(), // 나중에 하기
     });
     return (
         <ApolloProvider client={client}>
-            <Component {...pageProps} />
+            <Component />
         </ApolloProvider>
     );
 }
